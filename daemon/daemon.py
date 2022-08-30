28eb4e898aa5d88e82bcc145e7fddca136cc29aa
Dmytro Shestakov <altais@ukr.net>
2022-08-30 20:05:10 +0300

    Add set limits at startup

--- daemon/daemon.py
+++ daemon/daemon.py
@@ -1,10 +1,43 @@
 #! /usr/bin/python
 
 import serial
+import configparser
+
+conf = configparser.ConfigParser()
+conf.read('ups.conf')
+
+ser = serial.Serial('/dev/' + conf.get('UPS', 'Tty', fallback='ttyACM0'), timeout=5)
 
 
+def send_command(*cmd_with_args):
+    cmd = ' '.join(map(str, cmd_with_args))
+    ser.write(b'\x03\r')
+    ser.reset_input_buffer()
+    ser.write(bytes(f'{cmd}\r', 'utf8'))
+    ser.readline()
+    ser.readline()
+    return ser.readline()[:-2].decode('utf8')
 
-ser = serial.Serial('/dev/ttyACM0', timeout=5)
+
+def set_limits():
+    ups = conf['UPS']
+    print("Limit charge >", end=' ')
+    if conf.has_option('UPS', 'LimitCharge'):
+        res = send_command('limit-charge', ups['LimitCharge'])
+        print(res)
+    else:
+        print("As is, no overwrite")
+    print("Limit idle discharge >", end=' ')
+    if conf.has_option('UPS', 'LimitIdleDischarge'):
+        res = send_command('limit-discharge', ups['LimitIdleDischarge'])
+        print(res)
+    else:
+        print("As is, no overwrite")
+
+
+set_limits()
+exit(0)
+
 ser.write(b'\x03\r')
 ser.reset_input_buffer()
 ser.write(b'poll\r')
