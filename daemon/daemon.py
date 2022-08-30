d03192e8f0d93e0ba52b534cbfdb78ed1c3667a3
Dmytro Shestakov <altais@ukr.net>
2022-08-30 21:00:23 +0300

    Add shutdown trigger

--- daemon/daemon.py
+++ daemon/daemon.py
@@ -2,21 +2,43 @@
 
 import serial
 import configparser
+import signal
 
 conf = configparser.ConfigParser()
 conf.read('ups.conf')
 
-ser = serial.Serial('/dev/' + conf.get('UPS', 'Tty', fallback='ttyACM0'), timeout=5)
+ser = serial.Serial('/dev/' + conf.get('UPS', 'Tty', fallback='ttyACM0'), timeout=0.05)
+MAX_INPUT_LEN = 512
+
+
+def signal_handler(sig, frame):
+    print(' SIGINT captured')
+    exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+def port_is_alive():
+    try:
+        ser.inWaiting()
+        return True
+    except serial.SerialException:
+        return False
 
 
 def send_command(*cmd_with_args):
+    if not port_is_alive():
+        exit(-1)
     cmd = ' '.join(map(str, cmd_with_args))
     ser.write(b'\x03\r')
     ser.reset_input_buffer()
     ser.write(bytes(f'{cmd}\r', 'utf8'))
     ser.readline()
     ser.readline()
-    return ser.readline()[:-2].decode('utf8')
+    output = ser.read(MAX_INPUT_LEN).decode('utf8')
+    output = output[0:output.rfind('\r\n')]
+    return output
 
 
 def set_limits():
@@ -35,19 +57,26 @@ def set_limits():
         print("As is, no overwrite")
 
 
+print(send_command("info"))
 set_limits()
-exit(0)
 
-ser.write(b'\x03\r')
-ser.reset_input_buffer()
-ser.write(b'poll\r')
-while len(ser.readline().split()) != 4:
-    pass
+shutdown_threshold = conf.getint('UPS', 'ShutdownThreshold', fallback=30)
+print(f'Shutdown will be initiated reaching {shutdown_threshold}% battery level during discharge')
+
+
+def shutdown():
+    print('Shutdown started')
+
+
+send_command('poll')
+ser.timeout = 2
 while True:
-    v12, vbat, diff, status = ser.readline().split()
+    v12, vbat, diff, percent, status = ser.readline().split()
     v12 = int(v12)
     vbat = int(vbat)
     diff = int(diff)
+    percent = int(percent[:-1])
     status = status.decode('utf8')
-    print(v12, vbat, diff, status)
-ser.close()
+    if status == 'DISCHARGE' and percent <= shutdown_threshold:
+        shutdown()
+    print(v12, vbat, diff, percent, status)
