4d98d4f4e7d0cde04f230a6280001fdf250abc8a
Dmytro Shestakov <altais@ukr.net>
2022-08-31 20:28:59 +0300

    Various fixes and tweaks after the runtime testing

--- daemon/install.sh
+++ daemon/install.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/sh
 
-pip install -r ./requirements.txt
+if [ "$1" != "update" ]; then
+  pip install -r ./requirements.txt
+else
+  sudo systemctl disable --now ups-daemon
+fi
+
 sudo cp ./ups-daemon.conf /etc/
-chmod +x ./ups-daemon.py
-sudo cp ./ups-daemon.py /usr/local/sbin/
+chmod +x ./ups-daemon
+sudo cp ./ups-daemon /usr/local/sbin/
 sudo cp ./ups-daemon.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable --now ups-daemon
-
